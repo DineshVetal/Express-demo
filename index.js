fb1006520f5d8b54e0083ee404f89c671c6dd69f
unknown <vetaldinu@gmail.com>
2020-12-11 17:56:22 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const config = require('config')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const Joi = require('joi')
+const logger = require('./middleware/logger')
+const courses = require('./routes/courses')
+const home = require('./routes/home')
+const authenticator = require('./middleware/autenticator')
+const express = require('express');
+const app = express();
+
+// console.log(`NODE_ENV: ${process.env.NODE_ENV} `)
+// console.log(`app : ${ app.get('env') }`)
+
+app.use(express.json())
+app.use(express.urlencoded( { extended : true} ))
+app.use(express.static('public'))
+app.use(helmet())
+app.use('/api/courses', courses )
+app.use('/', home)
+
+// console.log(`Application name : ${config.get('name')}` )
+// console.log(`Mail server : ${config.get('mail.host')}` )
+// console.log(`Mail password : ${config.get('mail.password')} `)
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'))
+    console.log('Morgan Enabled......')
+    console.log(app.get('env'))
+}
+
+app.use( logger)
+app.use( authenticator)
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`Listening on port ${ port }...`));
